bec418652059979a558c91820073c321c88a5006
Headsman-4899 <dauletkd01@gmail.com>
2021-09-12 19:58:41 +0600

    Tour of Go

---
+++ week2/LoopsAndFunctions.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+}
+
+func main() {
+	var input int
+	fmt.Scanln(&input)
+
+	fmt.Println(Sqrt(float64(input)))
+}
