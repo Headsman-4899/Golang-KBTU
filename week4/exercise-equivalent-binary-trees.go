fcb9d53854bdd6e59052e80e16711c4301e8fec8
Headsman-4899 <dauletkd01@gmail.com>
2021-09-29 00:04:03 +0600

    hw3

---
+++ week4/exercise-equivalent-binary-trees.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"golang.org/x/tour/tree";
+	"fmt"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+	
+	ch <- t.Value
+	
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+ 	ch1 := make(chan int)
+ 	ch2 := make(chan int)
+	
+ 	go Walk(t1, ch1)
+ 	go Walk(t2, ch2)
+	
+	for i := 0; i < 10; i++ {
+		if <-ch1 != <-ch2 {
+   			return false
+  		}
+ 	}
+	return true
+}
+
+func main() {
+	ch := make(chan int, 10)
+	go Walk(tree.New(1), ch)
+	for i := 0; i < 10; i++ {
+		fmt.Print(<-ch)
+	}
+	
+	fmt.Println()
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
---
+++ week4/exersise-web-crawler.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type SafeCounter struct {
+	v  map[string]bool
+	mu sync.Mutex
+}
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+var c SafeCounter = SafeCounter{v: make(map[string]bool)}
+
+func (s *SafeCounter) IsUrlFound(url string) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	_, ok := s.v[url]
+	if !ok {
+		s.v[url] = true
+		return false
+	}
+	return true
+}
+
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher) {
+	// TODO: Fetch URLs in parallel.
+	// TODO: Don't fetch the same URL twice.
+	// This implementation doesn't do either:
+
+	// var c SafeCounter = SafeCounter{v: make(map[string]bool)}
+	if c.IsUrlFound(url) {
+		return
+	}
+	if depth <= 0 {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		go Crawl(u, depth-1, fetcher)
+	}
+	return
+}
+
+func main() {
+	Crawl("http://golang.org/", 4, fetcher)
+	time.Sleep(5 * time.Second)
+}
+
+// fakeFetcher is Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"http://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"http://golang.org/pkg/",
+			"http://golang.org/cmd/",
+		},
+	},
+	"http://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/cmd/",
+			"http://golang.org/pkg/fmt/",
+			"http://golang.org/pkg/os/",
+		},
+	},
+	"http://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/pkg/",
+		},
+	},
+	"http://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/pkg/",
+		},
+	},
+}
